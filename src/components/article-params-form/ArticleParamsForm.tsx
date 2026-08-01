e89623700e1002f6ac875d6efd23df1b859fe5eb
Pchola <zhosan.e-1@yandex.ru>
2026-08-01 20:51:12 +0300

    wefwefwef

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,154 @@
+import { useEffect, useRef, useState } from 'react';
+
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	ArticleStateType,
+} from 'src/constants/articleProps';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	onApply: (state: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	onApply,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] = useState(articleState);
+	const asideRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				isOpen &&
+				asideRef.current &&
+				!asideRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
+	useEffect(() => {
+		setFormState(articleState);
+	}, [articleState]);
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		onApply(formState);
+	};
+
+	const handleReset = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setFormState(defaultArticleState);
+		onApply(defaultArticleState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen((prev) => !prev)} />
+
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								fontFamilyOption: option,
+							}))
+						}
+					/>
+
+					<RadioGroup
+						title='Размер шрифта'
+						name='font-size'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								fontSizeOption: option,
+							}))
+						}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								fontColor: option,
+							}))
+						}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								backgroundColor: option,
+							}))
+						}
+					/>
+
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) =>
+							setFormState((prev) => ({
+								...prev,
+								contentWidth: option,
+							}))
+						}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
+
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
